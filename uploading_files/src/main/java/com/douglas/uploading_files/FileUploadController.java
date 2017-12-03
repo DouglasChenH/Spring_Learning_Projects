9bc69f7bda86163b2672f919364425ab472d4aca
DouglasChen <douglas.chenh@outlook.com>
2017-12-02 21:36:45 -0500

    create uploading_files project

---
+++ uploading_files/src/main/java/com/douglas/uploading_files/FileUploadController.java
@@ -0,0 +1,76 @@
+package com.douglas.uploading_files;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.douglas.uploading_files.storage.StorageFileNotFoundException;
+import com.douglas.uploading_files.storage.StorageService;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Controller
+public class FileUploadController {
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    //GET / looks up the current list of uploaded files from the StorageService
+    // and loads it into a Thymeleaf template.
+    // It calculates a link to the actual resource using MvcUriComponentsBuilder
+    @GetMapping("/")
+    public  String listUploadedFiles(Model model) throws IOException {
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+        return "uploadForm";
+
+    }
+
+    //GET /files/{filename} loads the resource if it exists,
+    // nd sends it to the browser to download using a "Content-Disposition" response header
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    //POST / is geared to handle a multi-part message file
+    // and give it to the StorageService for saving
+    @PostMapping("/")
+    public String handleFileUpload(@RequestParam("file")MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
+    }
+
+
+
+
+
+
+
+}
---
+++ uploading_files/src/main/java/com/douglas/uploading_files/UploadingFilesApplication.java
@@ -0,0 +1,27 @@
+package com.douglas.uploading_files;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.douglas.uploading_files.storage.StorageProperties;
+import com.douglas.uploading_files.storage.StorageService;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
